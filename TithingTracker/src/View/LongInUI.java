525e28d543729d18bda1769c33428ac860deed1d
elfreadmin <elfreadmin@CO-29.byui.edu>
2014-07-26 18:09:50 -0600

    Major issues in login fixed

--- TithingTracker/src/View/LongInUI.java
+++ TithingTracker/src/View/LongInUI.java
@@ -9,10 +9,18 @@ package View;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -33,24 +41,96 @@ public class LongInUI extends Application {
         primaryStage.setY(screenBounds.getMinY());
         primaryStage.setHeight(screenBounds.getHeight());
         primaryStage.setWidth(screenBounds.getWidth());
+
+        // Setting the title of the Stage
+        primaryStage.setTitle("Sign In");
+
+        // Creating the first element to be added to the Stage
+        StackPane root = new StackPane();
         
+        // Creating a grid to insert objects to it
+        GridPane pane = new GridPane();     
+        // setting the lines of the grids visible
+        //pane.setGridLinesVisible(true);
+        // Setting the grid to be center
+        pane.setAlignment(Pos.CENTER);
+        // Setting the vertical and horizontal gap
+        pane.setVgap(5);
+        pane.setHgap(5);
         
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+        // One text to display error messages
+        Text errorMessageText = new Text();
+        // setting the color to Red
+        errorMessageText.setFill(Color.FIREBRICK);
+        HBox boxForErrorMessage = new HBox();
+        boxForErrorMessage.getChildren().add(errorMessageText);
+        boxForErrorMessage.setAlignment(Pos.BASELINE_RIGHT);
+        pane.add(boxForErrorMessage, 1, 4);
+        
+        // Adding lables for the display
+        Label userLabel = new Label("User Name:");
+        TextField usernameField = new TextField();
+        pane.add(userLabel, 0, 0);
+        pane.add(usernameField, 0, 1);
+        
+        Label passwordLabel = new Label("Password:");
+        TextField passwordField = new TextField();
+        pane.add(passwordLabel, 1, 0);
+        pane.add(passwordField, 1, 1);
+        
+        //The user signs up by pressing this button
+        Button signUpButton = new Button();
+        signUpButton.setText("Sign Up");
+        signUpButton.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                // System.out.println("Sign Up Pressed");
+                  errorMessageText.setText("Sign Up Pressed");
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // The box allows me to position the button left, center or right
+        HBox boxForSignUpButton = new HBox();
+        boxForSignUpButton.setAlignment(Pos.BASELINE_LEFT);
+        boxForSignUpButton.getChildren().add(signUpButton);
+        
+        //pane.add(boxForSignUpButton, 0, 3);
+        
+        //The user logs in by pressing this button
+        Button loginButton = new Button();
+        loginButton.setText("Login");
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // System.out.println("Login pressed");
+                errorMessageText.setText("login Button Pressed");
+            }
+        });
+        
+        // It also allows me to place 2 objects center or sepate
+        HBox mainBoxForButtons = new HBox();
+        mainBoxForButtons.setAlignment(Pos.BASELINE_RIGHT);
+        // setting padding
+        mainBoxForButtons.setSpacing(10);
+        
+        HBox boxForLoginButton  = new HBox();
+        boxForLoginButton.setAlignment(Pos.BASELINE_RIGHT);
+        boxForLoginButton.getChildren().add(loginButton);
+        
+        // You can just add the button and you don't 
+        // need the boxes to do the same
+        // mainBoxForButtons.getChildren().addAll(signUpButton, loginButton);
+        
+        mainBoxForButtons.getChildren().addAll(boxForSignUpButton, boxForLoginButton);
+        
+        //          object, Column, Row
+        pane.add(mainBoxForButtons, 1, 3);
+        
+        root.getChildren().add(pane);
         
         Scene scene = new Scene(root, 500, 250);
         
-        primaryStage.setTitle("Hello World!");
+        // Final adding the scene to the stage
         primaryStage.setScene(scene);
         primaryStage.show();
     }
