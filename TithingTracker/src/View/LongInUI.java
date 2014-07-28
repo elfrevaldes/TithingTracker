43687e939ca524b9b6c536ff8edcd3528fd0693a
Tyson And Jenn <Tyson And Jenn@TysonAndJenn>
2014-07-28 12:31:49 -0700

    I changed around the positions of the buttons. I watched some videos on storing input. But it seems like we already accounted for that with the text fields. I will try to make the sign up button store the values typed into the text boxes to a specific variable tomorrow.

--- TithingTracker/src/View/LongInUI.java
+++ TithingTracker/src/View/LongInUI.java
@@ -7,6 +7,7 @@
 package View;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
